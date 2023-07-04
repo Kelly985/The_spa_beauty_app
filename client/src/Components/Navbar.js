c73fbefcc6f2cb1619051522f230d02f189dd26f
MORGANJASON <420morganjason@gmail.com>
2023-07-04 13:27:44 +0300

    styled navbar

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,20 +1,30 @@
 import React from 'react'
+
+
 function Navbar() {
   return (
-    <nav>
-      <a href='/' className='spa-title'>SPA BEAUTY</a>
-      <ul>
-        <li>
-          <a href='/home'>home</a>
-          <a href='/contact'>contact</a>
-          <a href='/appointment'>appointment</a>
-          <a href='/login'>login</a>
-          <a href='/sign up'>sign up</a>
-          
-          
-        </li>
-      </ul>
-    </nav>
+  <div>
+    <h1 className='spa-title'>  SPA BEAUTY </h1>
+  
+    
+<nav className='nav1'>
+  
+  <ul className='navlink-left'>
+    <li>
+      <a className='navlink' href='/home'>Home</a>
+      <a className='navlink' href='/contact'>Contact</a>
+      <a className='navlink' href='/appointment'>Appointment</a>
+    </li>
+  </ul>
+  <div className='dropdown'>
+    <span className='navlink-right dropdown-trigger'>Register</span>
+    <div className='dropdown-content'>
+      <a href='/signup'>Sign Up</a>
+      <a href='/login'>Login</a>
+    </div>
+  </div>
+</nav>
+</div>
   )
 }
 export default Navbar
