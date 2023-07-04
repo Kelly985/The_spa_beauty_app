9ae6976be60faa681a8509cccf9a38070762c2a6
James <kamaujames3705@gmail.com>
2023-07-04 11:30:29 +0300

    Navbar

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,11 +1,20 @@
 import React from 'react'
-
 function Navbar() {
   return (
-    <div>
-      
-    </div>
+    <nav>
+      <a href='/' className='spa-title'>SPA BEAUTY</a>
+      <ul>
+        <li>
+          <a href='/home'>home</a>
+          <a href='/contact'>contact</a>
+          <a href='/appointment'>appointment</a>
+          <a href='/login'>login</a>
+          <a href='/sign up'>sign up</a>
+          
+          
+        </li>
+      </ul>
+    </nav>
   )
 }
-
 export default Navbar
