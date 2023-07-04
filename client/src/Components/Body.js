dc99757ef05d8f59cddce86810493e020850b2c3
James <kamaujames3705@gmail.com>
2023-07-04 11:23:07 +0300

    Images

--- client/src/Components/Body.js
+++ client/src/Components/Body.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+
+
 
 function Body() {
   return (
     <div>
-      
+        <h1>Services</h1>
+        <div className='img'> 
+            <img className='nails_img' src='images/nails.jpg' alt='nails image'/>
+        </div>
+       
+    
     </div>
   )
 }
