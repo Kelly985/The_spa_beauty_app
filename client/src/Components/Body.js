02e8814e20b7e02dda8a827e9554801920664868
James <kamaujames3705@gmail.com>
2023-07-04 13:24:11 +0300

    Made cards for services

--- client/src/Components/Body.js
+++ client/src/Components/Body.js
@@ -4,14 +4,50 @@ import React from 'react';
 
 function Body() {
   return (
-    <div>
+    <div >
         <h1>Services</h1>
-        <div className='img'> 
+        <div className='cards'>
+        <div className='card1'> 
             <img className='nails_img' src='images/nails.jpg' alt='nails image'/>
+            </div>
+            <div className='card2'>
+            <img className='nails_img' src='images/barber.jpg' alt='barber image'/>
+            </div>
+            <div className='card3'>
+            <img className='nails_img' src='images/hrd.jpg' alt='hairdressing image'/>
+            </div>
+            <div className='card4'>
+            <img className='nails_img' src='images/massage.jpg' alt='massage image'/>
+            </div>
+            <div className='card5'>
+            <img className='nails_img' src='images/mkup.jpg' alt='makeup image'/>
+            </div>
+            <div className='card6'> 
+            <img className='nails_img' src='images/piercing.jpg' alt='piercing image'/>
+            </div>
+            <div className='card7'>
+            <img className='nails_img' src='images/tattoo.jpg' alt='tattoo image'/>
+            </div>
+            <div className='card8'>
+            <img className='nails_img' src='images/gym.jpg' alt='gym image'/>
+            </div>
+            <div className='card9'>
+            <img className='nails_img' src='images/sauna.jpg' alt='sauna image'/>
+            </div>
+            <div className='card10'>
+            <img className='nails_img' src='images/facial.jpg' alt='facial image'/>
+            </div>
+            <div className='card11'>
+            <img className='nails_img' src='images/pedicure.jpg' alt='pedicure image'/>
+            </div>
+            <div className='card12'>
+            <img className='nails_img' src='images/dental.jpg' alt='dental image'/>
+            </div>
+        </div>
         </div>
        
     
-    </div>
+   
   )
 }
 
